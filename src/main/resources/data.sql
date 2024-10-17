261e867506e988c35514803d40b0357c0bd69ca5
abhij2109 <abhij2116@gmail.com>
2024-10-18 00:18:29 +0530

    Added the data.sql file with SQL queries.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT into suppliers(name, address, reg_no) values("Abhi Jain","Hinjewadi Phase3",1001);
+INSERT into suppliers(name, address, reg_no) values("Ayush Jain","Nampally Hyderabad",1002);
+INSERT into suppliers(name, address, reg_no) values("Gungun Jain", "LaxmiChowk Delhi", 1003);
+INSERT into suppliers(name, address, reg_no) values("Anvi Jain", "Elight Chouraha Jhansi", 1004);
