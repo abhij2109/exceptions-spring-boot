6949d168859fb8b275d8a177e57b3eff637da844
abhij2109 <abhij2116@gmail.com>
2024-10-19 00:26:42 +0530

    correction in the column name.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT into suppliers(name, address, reg_no) values("Abhi Jain","Hinjewadi Phase3",1001);
-INSERT into suppliers(name, address, reg_no) values("Ayush Jain","Nampally Hyderabad",1002);
-INSERT into suppliers(name, address, reg_no) values("Gungun Jain", "LaxmiChowk Delhi", 1003);
-INSERT into suppliers(name, address, reg_no) values("Anvi Jain", "Elight Chouraha Jhansi", 1004);
+INSERT into suppliers(name, address, registrationNo) values("Abhi Jain","Hinjewadi Phase3",1001);
+INSERT into suppliers(name, address, registrationNo) values("Ayush Jain","Nampally Hyderabad",1002);
+INSERT into suppliers(name, address, registrationNo) values("Gungun Jain", "LaxmiChowk Delhi", 1003);
+INSERT into suppliers(name, address, registrationNo) values("Anvi Jain", "Elight Chouraha Jhansi", 1004);
